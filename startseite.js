45f4f5f7ac92400b83681445ad8ffe6e72c1626e
Ivorysanna <violetta.pyralov@web.de>
2021-01-07 21:43:25 +0100

    Produkte hinzufügen

--- startseite.js
+++ startseite.js
@@ -11,9 +11,21 @@ var Verleih;
         let result = await fetch("http://127.0.0.1:5001/Produkte");
         let produkte = JSON.parse(await result.text());
         for (let i = 0; i < produkte.length; i++) {
-            let divElement = document.createElement("div");
-            document.querySelector("#produkteDiv").appendChild(divElement);
-            divElement.innerHTML = produkte[i].name;
+            let produktDiv = document.createElement("div");
+            produktDiv.classList.add("produktDiv");
+            document.querySelector("#produkteContainer").appendChild(produktDiv);
+            let nameDiv = produktDiv.appendChild(document.createElement("div"));
+            nameDiv.classList.add("produktName");
+            nameDiv.innerHTML = produkte[i].name;
+            let bild = produktDiv.appendChild(document.createElement("img"));
+            bild.classList.add("produktBild");
+            bild.setAttribute("src", produkte[i].bild);
+            let beschreibungDiv = produktDiv.appendChild(document.createElement("div"));
+            beschreibungDiv.classList.add("produktBeschreibung");
+            beschreibungDiv.innerHTML = produkte[i].beschreibung;
+            let ausleihGebuehrDiv = produktDiv.appendChild(document.createElement("div"));
+            ausleihGebuehrDiv.classList.add("gebuehrenDiv");
+            ausleihGebuehrDiv.innerHTML = produkte[i].ausleihGebuehr.toString() + " €";
         }
     }
     produkteAnzeigen();
