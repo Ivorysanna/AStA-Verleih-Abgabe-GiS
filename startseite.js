04cc74d5644e82afb94c014c86ce822c1f22369f
Ivorysanna <violetta.pyralov@web.de>
2021-01-13 18:42:45 +0100

    Status eingebunden

--- startseite.js
+++ startseite.js
@@ -1,15 +1,14 @@
 "use strict";
 var Verleih;
 (function (Verleih) {
-    let Status;
-    (function (Status) {
-        Status[Status["FREI"] = 0] = "FREI";
-        Status[Status["AUSGELIEHEN"] = 1] = "AUSGELIEHEN";
-        Status[Status["RESERVIERT"] = 2] = "RESERVIERT";
-    })(Status || (Status = {}));
+    // enum Status {
+    //     FREI, AUSGELIEHEN, RESERVIERT
+    // }
     async function produkteAnzeigen() {
         let result = await fetch("http://127.0.0.1:5001/Produkte");
         let produkte = JSON.parse(await result.text());
+        console.log(produkte);
+        // Erstellen von Divs für Produkte
         for (let i = 0; i < produkte.length; i++) {
             let produktDiv = document.createElement("div");
             produktDiv.classList.add("produktDiv");
@@ -19,13 +18,16 @@ var Verleih;
             nameDiv.innerHTML = produkte[i].name;
             let bild = produktDiv.appendChild(document.createElement("img"));
             bild.classList.add("produktBild");
-            bild.setAttribute("src", produkte[i].bild);
+            bild.setAttribute("src", "Bilder/Objekte/" + produkte[i].bild);
             let beschreibungDiv = produktDiv.appendChild(document.createElement("div"));
             beschreibungDiv.classList.add("produktBeschreibung");
             beschreibungDiv.innerHTML = produkte[i].beschreibung;
             let ausleihGebuehrDiv = produktDiv.appendChild(document.createElement("div"));
             ausleihGebuehrDiv.classList.add("gebuehrenDiv");
             ausleihGebuehrDiv.innerHTML = produkte[i].ausleihGebuehr.toString() + " €";
+            let statusImg = produktDiv.appendChild(document.createElement("img"));
+            statusImg.classList.add("statusBild");
+            statusImg.setAttribute("src", "Bilder/" + produkte[i].status + ".png");
         }
     }
     produkteAnzeigen();
--- startseite.ts
+++ startseite.ts
@@ -7,18 +7,19 @@ namespace Verleih {
         beschreibung: string;
         bild: string;
         ausleihGebuehr: number;
-        status: Status;
+        status: string;
     }
 
-    enum Status {
-        FREI, AUSGELIEHEN, RESERVIERT
-    }
-
+    // enum Status {
+    //     FREI, AUSGELIEHEN, RESERVIERT
+    // }
 
     async function produkteAnzeigen() {
         let result: Response = await fetch("http://127.0.0.1:5001/Produkte");
         let produkte: Produkt[] = JSON.parse(await result.text());
+        console.log(produkte);
 
+        // Erstellen von Divs für Produkte
         for (let i: number = 0; i < produkte.length; i++) {
             let produktDiv = document.createElement("div");
             produktDiv.classList.add("produktDiv");
@@ -30,7 +31,7 @@ namespace Verleih {
 
             let bild = produktDiv.appendChild(document.createElement("img"));
             bild.classList.add("produktBild");
-            bild.setAttribute("src", produkte[i].bild);
+            bild.setAttribute("src", "Bilder/Objekte/" + produkte[i].bild);
             
             let beschreibungDiv = produktDiv.appendChild(document.createElement("div"));
             beschreibungDiv.classList.add("produktBeschreibung");
@@ -39,6 +40,12 @@ namespace Verleih {
             let ausleihGebuehrDiv = produktDiv.appendChild(document.createElement("div"));
             ausleihGebuehrDiv.classList.add("gebuehrenDiv");
             ausleihGebuehrDiv.innerHTML = produkte[i].ausleihGebuehr.toString() + " €";
+
+            let statusImg = produktDiv.appendChild(document.createElement("img"));
+            statusImg.classList.add("statusBild");
+            statusImg.setAttribute("src", "Bilder/" + produkte[i].status + ".png");
+
+        
         }
 
     }
