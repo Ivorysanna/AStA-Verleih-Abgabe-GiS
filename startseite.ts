447a4b39f353fd9fbe30dee9de5eda48928cd041
Ivorysanna <violetta.pyralov@web.de>
2021-01-20 21:22:37 +0100

    enum gelöscht

--- startseite.ts
+++ startseite.ts
@@ -2,7 +2,7 @@
 
 namespace Verleih {
 
-    interface Produkt {
+    export interface Produkt {
         _id: string;
         name: string;
         beschreibung: string;
@@ -11,48 +11,46 @@ namespace Verleih {
         status: string;
     }
 
-    // enum Status {
-    //     FREI, AUSGELIEHEN, RESERVIERT
-    // }
 
     async function produkteAnzeigen() {
         let result: Response = await fetch("http://127.0.0.1:5001/Produkte");
         let produkte: Produkt[] = JSON.parse(await result.text());
         console.log(produkte);
 
+
         // Erstellen von Divs für Produkte
         for (let i: number = 0; i < produkte.length; i++) {
-            let produktDiv = document.createElement("div");
+            let produktDiv: HTMLDivElement = document.createElement("div");
             produktDiv.classList.add("produktDiv");
 
             //Name reinladen
             document.querySelector("#produkteContainer").appendChild(produktDiv);
-            let nameDiv = produktDiv.appendChild(document.createElement("div"));
+            let nameDiv: HTMLDivElement= produktDiv.appendChild(document.createElement("div"));
             nameDiv.classList.add("produktName");
             nameDiv.innerHTML = produkte[i].name;
 
             //Bild reinladen
-            let bild = produktDiv.appendChild(document.createElement("img"));
+            let bild: HTMLImageElement = produktDiv.appendChild(document.createElement("img"));
             bild.classList.add("produktBild");
             bild.setAttribute("src", "Bilder/Objekte/" + produkte[i].bild);
 
             //Produktbeschreibung
-            let beschreibungDiv = produktDiv.appendChild(document.createElement("div"));
+            let beschreibungDiv: HTMLDivElement  = produktDiv.appendChild(document.createElement("div"));
             beschreibungDiv.classList.add("produktBeschreibung");
             beschreibungDiv.innerHTML = produkte[i].beschreibung;
             
             //Gebühren 
-            let ausleihGebuehrDiv = produktDiv.appendChild(document.createElement("div"));
+            let ausleihGebuehrDiv: HTMLDivElement = produktDiv.appendChild(document.createElement("div"));
             ausleihGebuehrDiv.classList.add("gebuehrenDiv");
             ausleihGebuehrDiv.innerHTML = produkte[i].ausleihGebuehr.toString() + " €";
 
             //Statusbild
-            let statusImg = produktDiv.appendChild(document.createElement("img"));
+            let statusImg: HTMLImageElement= produktDiv.appendChild(document.createElement("img"));
             statusImg.classList.add("statusBild");
             statusImg.setAttribute("src", "Bilder/" + produkte[i].status + ".png");
 
             //Button für Warenkorb
-            let warenkorbButton = produktDiv.appendChild(document.createElement("button"));
+            let warenkorbButton: HTMLButtonElement = produktDiv.appendChild(document.createElement("button"));
             warenkorbButton.classList.add("warenkorbButton");
             warenkorbButton.setAttribute("type", "button");
             warenkorbButton.innerHTML = "Reservieren";
@@ -75,8 +73,8 @@ namespace Verleih {
         function addWarenkorb(_event: Event): void{
             //Produkte selektieren durch Button druck
             let target: HTMLElement = <HTMLElement>_event.target;
-            let index = parseInt(target.getAttribute("ArtikelIndex"));
-            let ausgewaehlterArtikel: Produkt = (produkte[index]);
+            let index: number = parseInt(target.getAttribute("ArtikelIndex"));
+            let ausgewaehlterArtikel: Produkt = produkte[index];
 
 
             //Abfrage ob Artikel vorhanden ist
@@ -85,7 +83,7 @@ namespace Verleih {
                 let warenkorb: Produkt[] = JSON.parse(localStorage.getItem(warenkorbLocalStorage));
                 console.log(warenkorb);
 
-                let istImWarenkorb = false;
+                let istImWarenkorb: boolean = false;
                 for(let i: number = 0; i < warenkorb.length; i++){
                     if(warenkorb[i]._id == ausgewaehlterArtikel._id){
                         istImWarenkorb = true;
