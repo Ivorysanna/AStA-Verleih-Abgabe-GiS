998cf6e7851b9b97bc0808d0b6eda0633b8f659d
Ivorysanna <violetta.pyralov@web.de>
2021-01-20 21:22:14 +0100

    Löschen von test,.ts

--- test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-async function sendOrder() {
-    let result = await fetch("http://127.0.0.1:5001/Produkte");
-    console.log(await result.text());
-}
-sendOrder();
-//# sourceMappingURL=test.js.map
--- test.ts
+++
@@ -1,6 +0,0 @@
-async function sendOrder(){
-    let result: Response = await fetch("http://127.0.0.1:5001/Produkte");
-    console.log(await result.text());
-}
-
-sendOrder();
