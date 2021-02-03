892c58ee17f1e96249d446386108d9b3c22bd885
Ivorysanna <violetta.pyralov@web.de>
2021-02-03 19:50:48 +0100

    Produkte werden geladen mit Button Name der Person fehlt noch
    Button zum freigeben und als Ausgeliehen Markieren

--- interneSeite.js
+++ interneSeite.js
@@ -1,47 +1,44 @@
-namespace Verleih {
+"use strict";
+var Verleih;
+(function (Verleih) {
     async function produkteAstaAnzeigen() {
-        let result: Response = await fetch("http://127.0.0.1:5001/AstaIntern");
-        let produkte: Produkt[] = JSON.parse(await result.text());
+        let result = await fetch("http://127.0.0.1:5001/AstaIntern");
+        let produkte = JSON.parse(await result.text());
         console.log(produkte);
-
-        for (let i: number = 0; produkte.length; i++) {
-            let produktDiv: HTMLDivElement = document.createElement("div");
+        for (let i = 0; produkte.length; i++) {
+            let produktDiv = document.createElement("div");
             produktDiv.classList.add("produktDiv");
-
             //Name reinladen
             document.querySelector(".reservierteProdukte").appendChild(produktDiv);
-            let nameDiv: HTMLDivElement = produktDiv.appendChild(document.createElement("div"));
+            let nameDiv = produktDiv.appendChild(document.createElement("div"));
             nameDiv.classList.add("produktName");
-            nameDiv.innerHTML = produkte[i].name
-
+            nameDiv.innerHTML = produkte[i].name;
             //Bild reinladen
-            let bild: HTMLImageElement = produktDiv.appendChild(document.createElement("img"));
+            let bild = produktDiv.appendChild(document.createElement("img"));
             bild.classList.add("produktBild");
             bild.setAttribute("src", "Bilder/Objekte/" + produkte[i].bild);
-
             //Gebühren
-            let gebuehrenDiv: HTMLDivElement = produktDiv.appendChild(document.createElement("div"));
+            let gebuehrenDiv = produktDiv.appendChild(document.createElement("div"));
             gebuehrenDiv.classList.add("gebuehrenDiv");
             gebuehrenDiv.innerHTML = produkte[i].ausleihGebuehr.toString() + " €";
-
             //Statusbild
-            let statusImg: HTMLImageElement = produktDiv.appendChild(document.createElement("img"));
+            let statusImg = produktDiv.appendChild(document.createElement("img"));
             statusImg.classList.add("statusBild");
             statusImg.setAttribute("src", "Bilder/" + produkte[i].status + ".png");
-            
-            if(produkte[i].status == "frei"){
+            if (produkte[i].status == "frei") {
                 //mach mal nichts
-            }else if (produkte[i].status =="reserviert"){
+            }
+            else if (produkte[i].status == "reserviert") {
                 //Button für ausgeliehen wechseln
-                let ausgeliehenButton: HTMLButtonElement = produktDiv.appendChild(document.createElement("button"));
+                let ausgeliehenButton = produktDiv.appendChild(document.createElement("button"));
                 ausgeliehenButton.classList.add("ausgeliehenButton");
                 ausgeliehenButton.setAttribute("type", "button");
                 ausgeliehenButton.innerHTML = "Ausgeliehen";
                 ausgeliehenButton.setAttribute("ArtikelIndex", i.toString());
-
-            }else{
+            }
+            else {
                 //Button wieder freigeben
-                let freigebenButton: HTMLButtonElement = produktDiv.appendChild(document.createElement("button"));
+                let freigebenButton = produktDiv.appendChild(document.createElement("button"));
                 freigebenButton.classList.add("freigebenButton");
                 freigebenButton.setAttribute("type", "button");
                 freigebenButton.innerHTML = "Wieder freigeben";
@@ -49,6 +46,6 @@ namespace Verleih {
             }
         }
     }
-
     produkteAstaAnzeigen();
-}
+})(Verleih || (Verleih = {}));
+//# sourceMappingURL=interneSeite.js.map
