7a83a00bb2baa490e273ba02f765bab880066687
Ivorysanna <violetta.pyralov@web.de>
2021-02-04 15:05:42 +0100

    Global erstellen mit url

---
+++ global.js
@@ -0,0 +1,6 @@
+"use strict";
+var Verleih;
+(function (Verleih) {
+    Verleih.serverUrl = "http://127.0.0.1:5001/";
+})(Verleih || (Verleih = {}));
+//# sourceMappingURL=global.js.map
---
+++ global.ts
@@ -0,0 +1,3 @@
+namespace Verleih{
+    export const serverUrl: string = "http://127.0.0.1:5001/";
+}
