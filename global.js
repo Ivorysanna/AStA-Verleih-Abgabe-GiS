9cffdd263944f99853ce2ed2cad55dbbf82c17b2
Ivorysanna <violetta.pyralov@web.de>
2021-02-06 15:21:06 +0100

    global URL von Heroku

--- global.js
+++ global.js
@@ -1,7 +1,8 @@
 "use strict";
 var Verleih;
 (function (Verleih) {
-    Verleih.serverUrl = "http://127.0.0.1:5001/";
+    Verleih.serverUrl = "https://giswise.herokuapp.com/";
+    // export const serverUrl: string = "http://127.0.0.1:5001/";
     Verleih.warenkorbLocalStorage = "warenkorb";
 })(Verleih || (Verleih = {}));
 //# sourceMappingURL=global.js.map
--- global.ts
+++ global.ts
@@ -1,4 +1,5 @@
 namespace Verleih{
-    export const serverUrl: string = "http://127.0.0.1:5001/";
+    export const serverUrl: string = "https://giswise.herokuapp.com/";
+    // export const serverUrl: string = "http://127.0.0.1:5001/";
     export const warenkorbLocalStorage: string = "warenkorb";
 }
